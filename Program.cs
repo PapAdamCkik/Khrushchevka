c3b8bc16a743eead20c01e70dffa628ab28d73c8
PapAdamCkik <pap.adam@ckik.hu>
2026-02-11 19:50:39 +0100

    first_commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 450, "Hello Raylib-cs");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.RayWhite);
+    Raylib.DrawText("Hello from Raylib-cs!", 190, 200, 20, Color.Black);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
